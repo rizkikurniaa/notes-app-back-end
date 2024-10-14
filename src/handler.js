8e4cc08b7734b118ea227df252838f71377d3fe4
rizki <rizkikurniawan1797@gmail.com>
2024-10-14 10:48:27 +0700

    init

---
+++ src/handler.js
@@ -0,0 +1,19 @@
+const { nanoid } = require('nanoid');
+const notes = require('./notes');
+
+const addNoteHandler = (request) => {
+  const { title, tags, body } = request.payload;
+
+  const id = nanoid(16);
+  const createdAt = new Date().toISOString();
+  const updatedAt = createdAt;
+
+  const newNote = {
+    title, tags, body, id, createdAt, updatedAt,
+  };
+
+  notes.push(newNote);
+
+};
+
+module.exports = { addNoteHandler };
